3a1f51b7b1c553c5884617d9ebbd1bc33b81df43
insanetron75 <timmy_hasler@hotmail.com>
2018-04-23 19:32:50 +0100

    Insert Genus entity - next is Doctrine tutorial 4

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -9,6 +10,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new", name="")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName("Octopus".rand(1, 100));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>genus created</body></html>');
+
+    }
+
     /**
      * @Route("/genus/{genusName}")
      * @param string $genusName
